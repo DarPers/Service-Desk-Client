aaf749f61a6f31adc966d0bd675b8241b906184a
DarPers <dariaerohova21@gmail.com>
2024-05-17 09:08:18 +0200

    Revert "init"
    This reverts commit 0e2ff3937bdef48a583a6fafc3c3c6930f38a32b.

--- src/api.ts
+++
@@ -1,3 +0,0 @@
-export const baseUrl = "http://localhost:8000";
-export const ticketRequest = "/tickets";
-export const userRequest = "/tickets";
--- src/enums/enums.ts
+++
@@ -1,7 +0,0 @@
-export enum TicketType {
-    None = 0,
-    Free = 1,
-    InProgress = 2,
-    Ready = 3,
-    All = 4,
-}
